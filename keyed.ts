0e99ce8612c26fc9341f80756c3c014ece601ba3
Martin Jurča <mjurca@centrum.cz>
2018-12-31 09:11:25 +0100

    added a runtime type check to the keyed template tag

--- keyed.ts
+++ keyed.ts
@@ -1,6 +1,10 @@
 import TemplateData from './TemplateData.js'
 
 export default function keyed(key: any): (templateSource: string[], ...placeholderValues: any[]) => TemplateData {
+    if (key === undefined) {
+        throw new TypeError('The key cannot be undefined. Specify a valid key or use the tpl template tag instead.')
+    }
+
     return function tpl(templateSource: string[], ...placeholderValues: any[]): TemplateData {
         return new TemplateData(key, templateSource, placeholderValues)
     }
